dc6867955cda74691c6300ac69351ed49a126797
Ernest Surudo <ernest.surudo@gmail.com>
2017-05-05 12:04:12 +0200

    Fixed merge bug

--- lib/workflow/adapters/active_record.rb
+++ lib/workflow/adapters/active_record.rb
@@ -50,8 +50,8 @@ module Workflow
           end
         end
 
-        def workflow_with_scopes(&specification)
-          workflow_without_scopes(&specification)
+        def workflow_with_scopes(options = {}, &specification)
+          workflow_without_scopes(options, &specification)
           states = workflow_spec.states.values
 
           states.each do |state|
